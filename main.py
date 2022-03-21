0564d1cc763914ddc846c10775bafea832659865
overheavenlyshadows <v.merkulov@bi.zone>
2022-03-22 01:26:06 +0300

    some fixes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 from ast import Try
-from asyncio.subprocess import STDOUT
+from asyncio.subprocess import DEVNULL, STDOUT
 import subprocess
 import argparse
 import re
@@ -11,7 +11,6 @@ import sys, os
 def args_parse():
     global argparser
     argparser = argparse.ArgumentParser(description="""
-    Usage:
     massnmap <IP/Range or args for list> <masscan args> --nm <nmap args>
     """)
     argparser.add_argument('--nm', help='Delimeter between masscan and nmap args', required=True, action='store_true')
@@ -32,22 +31,22 @@ def args_parse():
 def args_check():
     try:
         subprocess.check_output(['masscan', *masscan_args], stderr=STDOUT)
-    except Exception as log:
-        if (re.search(r".*unknown config option.*", (log.output).decode('utf-8')) is not None):
+    except Exception as error:
+        if (re.search(r".*unknown config option.*", (error.output).decode('utf-8')) is not None):
             print('Masscan args are invalid:')
-            print(log.output.decode('utf-8'))
+            print(error.output.decode('utf-8'))
             quit()
     try:
         subprocess.check_output(['nmap', *nmap_args], stderr=STDOUT)
-    except Exception as log:
-        if (re.search(r".*unrecognized option.*", (log.output).decode('utf-8')) is not None):
+    except Exception as error:
+        if (re.search(r".*unrecognized option.*", (error.output).decode('utf-8')) is not None):
             print('Nmap args are invalid:')
-            print(log.output.decode('utf-8'))
+            print(error.output.decode('utf-8'))
             quit()
 
         
 def masscan_run():
-    # try:
+    try:
         nmap_input = {}
         masscan_output = subprocess.check_output(['masscan', target_ip, *masscan_args]).decode('utf-8')
         for line in masscan_output.splitlines():
@@ -58,6 +57,9 @@ def masscan_run():
             else:
                 nmap_input[ip].append(port)
         nmap_run(nmap_input)
+    except Exception as error:
+        print('Unexcpected Error:')
+        print(error.output)
 
 
 def nmap_run(nmap_output):
@@ -71,8 +73,9 @@ def nmap_run(nmap_output):
             port_list.append(port)
         port_list_ready = delim.join(map(str, port_list)) #unpack tuple with delimiter
         print('nmap', target_ip, '-p'+port_list_ready, *nmap_args)
-        nmap_output = subprocess.check_output(['nmap', target_ip, '-p'+port_list_ready, *nmap_args]).decode('utf-8')
+        nmap_output = subprocess.check_output(['nmap', target_ip, '-p'+port_list_ready, *nmap_args], stderr=DEVNULL).decode('utf-8')
         print(nmap_output)
+        print('\n----------------------------------------------------\n')
              
         # if re.search(r"tcp", ports[0]:
         #     nmap_mode = 'tcp'
